51251b81aabea9ba3b6ad71f14f4fc04698ca34a
c4605 <bolasblack@gmail.com>
2020-06-27 22:46:01 +0800

    test(ts-types): fix dtslint error in ts 4.0

--- packages/ts-types/dtslint/Fn.ts
+++ packages/ts-types/dtslint/Fn.ts
@@ -2,11 +2,11 @@ import { ArgsType, FstArgType, SndArgType, ThdArgType } from '@c4605/ts-types'
 
 // $ExpectType []
 type ArgsTypeT1 = ArgsType<() => void>
-// $ExpectType [1]
+// $ExpectType [a: 1]
 type ArgsTypeT2 = ArgsType<(a: 1) => void>
-// $ExpectType [1, 2]
+// $ExpectType [a: 1, c: 2]
 type ArgsTypeT3 = ArgsType<(a: 1, c: 2) => void>
-// $ExpectType [1, 2, ...boolean[]]
+// $ExpectType [a: 1, c: 2, ...args: boolean[]]
 type ArgsTypeT4 = ArgsType<(a: 1, c: 2, ...args: boolean[]) => void>
 // $ExpectError
 const argsTypeV2: ArgsType<(a: 1, b: 2) => void> = [1, 3]
